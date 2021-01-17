cdb167c70ffa098a40fa280916161ca39f76f052
osidekyle <hawkinskyle67@gmail.com>
2021-01-17 13:10:51 -0800

    fin

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose=require("mongoose")
 const dotenv=require("dotenv")
 const morgan = require("morgan")
 const exphbs=require("express-handlebars")
+const methodOverride = require("method-override")
 const passport=require("passport")
 const session=require("express-session")
 const MongoStore=require("connect-mongo")(session)
@@ -24,16 +25,25 @@ const app=express()
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
+//Method override
+app.use(methodOverride(function (req,res){
+    if(req.body && typeof req.body==="object" && "_method" in req.body){
+        var method=req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
 if(process.env.NODE_ENV==='development'){
     app.use(morgan('dev'))
 }
 
 //Hbs Helpers
-const {formatDate,truncate,stripTags,editIcon}=require("./helpers/hbs")
+const {formatDate,truncate,stripTags,editIcon,select}=require("./helpers/hbs")
 
 
 //Handlebars
-app.engine(".hbs",exphbs({helpers:{formatDate,editIcon, truncate,stripTags},defaultLayout:"main",extname:".hbs"}))
+app.engine(".hbs",exphbs({helpers:{formatDate,editIcon, truncate,stripTags,select},defaultLayout:"main",extname:".hbs"}))
 app.set("view engine", ".hbs")
 
 //Sesion middleware
